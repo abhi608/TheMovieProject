d0d2a362b1dcd170b052eebab9ff33a2efef3c0c
Abhishek Verma <608abhishek@gmail.com>
2022-04-18 22:03:26 -0400

    initial commit

---
+++ src/directorMapping/DirectorMapper.java
@@ -0,0 +1,23 @@
+package directorMapping;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+public class DirectorMapper extends Mapper<LongWritable, Text, Text, Text> {
+	Text k = new Text();
+	Text v = new Text();
+	
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String[] cols = value.toString().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+		k.set(key.toString());
+		v.set(Arrays.toString(cols));
+		context.write(k, v);
+	}
+	
+}
