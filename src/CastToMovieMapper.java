6a7bcc376eecca1c4a7eb276f2d3c5b4729ff55c
Abhishek Verma <608abhishek@gmail.com>
2022-04-19 19:36:45 -0400

    added cast to movie mapping code files

---
+++ src/CastToMovieMapper.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
+import net.minidev.json.parser.ParseException;
+
+public class CastToMovieMapper extends Mapper<LongWritable, Text, Text, Text> {
+	
+	Text k = new Text();
+	Text v = new Text();
+	
+	@Override
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String[] cols = value.toString().split("\t");
+		if(cols.length != 4) return;
+		String movieId = String.valueOf(cols[0]);
+		v.set(movieId);
+		String cast = cols[2];
+		cast = cast.replaceAll("\"\\[", "[");
+		cast = cast.replaceAll("\\]\"", "]");
+		cast = cast.replaceAll("\"\"", "\"");
+		try {
+			JSONArray root = (JSONArray) JSONValue.parseWithException(cast);
+			for(int i=0; i<root.size(); i++) {
+				JSONObject curCast = (JSONObject) root.get(i);
+				String castId = String.valueOf((int) curCast.get("id"));
+				k.set(castId);
+				context.write(k, v);
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
