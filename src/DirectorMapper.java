ff3a2403d7db29f738d96cd65d5a7810c1c175b1
Abhishek Verma <608abhishek@gmail.com>
2022-04-19 01:04:05 -0400

    mapper updated

--- src/DirectorMapper.java
+++ src/DirectorMapper.java
@@ -25,29 +25,29 @@ public class DirectorMapper extends Mapper<LongWritable, Text, Text, Text> {
 		crew = crew.replaceAll("\\]\"", "]");
 		crew = crew.replaceAll("\"\"", "\"");
 //		crew = "{\"data\": " + crew + "}";
-//		try {
-//			JSONArray root = (JSONArray) JSONValue.parseWithException(crew);
-//			for(int i=0; i<root.size(); i++) {
-//				JSONObject curCrew = (JSONObject) root.get(i);
-//				String job = (String) curCrew.get("job");
-//				job = job.replaceAll("\\s+","");
-//				if("director".equalsIgnoreCase(job)) {
-//					String id = String.valueOf((int) curCrew.get("id"));
-//					String name = (String) curCrew.get("name");
-//					k.set(id);
-//					v.set(name);
-//					context.write(k, v);
-//				}
-//			}
-//		} catch (ParseException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			JSONArray root = (JSONArray) JSONValue.parseWithException(crew);
+			for(int i=0; i<root.size(); i++) {
+				JSONObject curCrew = (JSONObject) root.get(i);
+				String job = (String) curCrew.get("job");
+				job = job.replaceAll("\\s+","");
+				if("director".equalsIgnoreCase(job)) {
+					String id = String.valueOf((int) curCrew.get("id"));
+					String name = (String) curCrew.get("name");
+					k.set(id);
+					v.set(name);
+					context.write(k, v);
+				}
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
-		k.set(Integer.toString(cols.length));
-		v.set(crew);
-		context.write(k, v);
+//		k.set(Integer.toString(cols.length));
+//		v.set(crew);
+//		context.write(k, v);
 	}
 	
 }
