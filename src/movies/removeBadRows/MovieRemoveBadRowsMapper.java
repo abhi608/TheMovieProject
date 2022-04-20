d061af6e18ae1d0df2a69f36c8db12c9800249ef
Abhishek Verma <608abhishek@gmail.com>
2022-04-20 16:19:17 -0400

    remove bad movie rows

--- src/movies/removeBadRows/MovieRemoveBadRowsMapper.java
+++ src/movies/removeBadRows/MovieRemoveBadRowsMapper.java
@@ -1,7 +1,5 @@
 package movies.removeBadRows;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
@@ -10,16 +8,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
 
 import com.google.common.base.Strings;
-
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
-import net.minidev.json.JSONValue;
-import net.minidev.json.parser.ParseException;
-
-enum Errors {
-	PARSEERROR,
-	ROWS_WITH_MISSING_COLUMNS
-}
 
 public class MovieRemoveBadRowsMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
 	
