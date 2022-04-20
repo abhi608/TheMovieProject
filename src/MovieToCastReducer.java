885a434e85d60af3c0f6b43a1fa69e24a0d449d4
Abhishek Verma <608abhishek@gmail.com>
2022-04-19 20:07:57 -0400

    movie to cast mapping code files

---
+++ src/MovieToCastReducer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class MovieToCastReducer extends Reducer<Text, Text, Text, Text> {
+	
+	Text v = new Text();
+	
+	@Override
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+		StringBuilder str = new StringBuilder();
+		String prefix = "";
+		for(Text t : values) {
+			str.append(prefix);
+			str.append(t.toString());
+			prefix = ",";
+			
+		}
+		v.set(str.toString());
+		context.write(key, v);
+	}
+}
+ 
